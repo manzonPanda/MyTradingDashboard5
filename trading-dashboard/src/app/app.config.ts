14954b32d63265b98f056a533c8cc717786b78e5
JAKE <manzonjake15@gmail.com>
2025-04-27 06:12:21 +0800

    added firebase

--- trading-dashboard/src/app/app.config.ts
+++ trading-dashboard/src/app/app.config.ts
@@ -5,6 +5,21 @@ import { routes } from './app.routes';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5-Z3aLRr-HyopLGF6kXDPR1DdOKoEI_Q",
+  authDomain: "tradingdashboard-fce7d.firebaseapp.com",
+  projectId: "tradingdashboard-fce7d",
+  storageBucket: "tradingdashboard-fce7d.firebasestorage.app",
+  messagingSenderId: "714112340582",
+  appId: "1:714112340582:web:a2709e6fbc6c9c33ee53a7",
+  measurementId: "G-1CZDEEM8R2"
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(
@@ -12,6 +27,8 @@ export const appConfig: ApplicationConfig = {
         provide: DateAdapter,
         useFactory: adapterFactory,
       })
-    )
+    ),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ]
 };
