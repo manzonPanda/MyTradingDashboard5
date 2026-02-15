068ca37738fe15e14e23a28528b4272a5e08d97c
mart <01rh.mart@gmail.com>
2026-02-15 19:21:44 +0800

    changed account id

--- trading-dashboard/src/environments/environment.ts
+++ trading-dashboard/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   // backendUrlNotion: 'http://localhost:3000',
   backendUrlNotion: 'https://notionproxyapi.onrender.com',
   backendUrlMt5: 'https://mt5-api.jakemt5.host',
-  propfirmAccountName: '5ers9️⃣2.5k [#25977457]',
+  propfirmAccountName: '5ers🔟2.5k [#26032524]',
   firebaseConfig: {
     apiKey: "AIzaSyB5-Z3aLRr-HyopLGF6kXDPR1DdOKoEI_Q",
     authDomain: "tradingdashboard-fce7d.firebaseapp.com",
