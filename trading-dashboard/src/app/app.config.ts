88a85e9e946c4d9cac9a88fb95a2e24c7d6e62d8
Jake James <manzonjake15@gmail.com>
2026-08-09 08:51:06 +0800

    fixed error build

--- trading-dashboard/src/app/app.config.ts
+++ trading-dashboard/src/app/app.config.ts
@@ -1,14 +1,9 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { provideFirebaseApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideHttpClient } from '@angular/common/http';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { environment } from '../environments/environment';
@@ -23,8 +18,6 @@ export const appConfig: ApplicationConfig = {
         useFactory: adapterFactory,
       })
     ),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => getFirestore()),
     provideHttpClient(),
     provideCharts(withDefaultRegisterables())
   ]
